bb482e5eaafed28578daf4e4d54364b43d73017f
Eganandan <SEganandan@ad.harman.com>
2018-01-09 16:02:51 +0530

    basic chat server UI

--- ChatServer/ChatServer/MainWindow.Designer.cs
+++ ChatServer/ChatServer/MainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChatServer
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbProcessLog = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbServerStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(266, 26);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(91, 28);
+            this.btnStartServer.TabIndex = 2;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Location = new System.Drawing.Point(372, 26);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(91, 28);
+            this.btnStopServer.TabIndex = 2;
+            this.btnStopServer.Text = "Stop Server";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Port:";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(190, 31);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(51, 20);
+            this.tbPort.TabIndex = 1;
+            // 
+            // tbProcessLog
+            // 
+            this.tbProcessLog.Location = new System.Drawing.Point(25, 107);
+            this.tbProcessLog.Multiline = true;
+            this.tbProcessLog.Name = "tbProcessLog";
+            this.tbProcessLog.ReadOnly = true;
+            this.tbProcessLog.Size = new System.Drawing.Size(438, 214);
+            this.tbProcessLog.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Log:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Server Status :";
+            // 
+            // lbServerStatus
+            // 
+            this.lbServerStatus.AutoSize = true;
+            this.lbServerStatus.Location = new System.Drawing.Point(346, 91);
+            this.lbServerStatus.Name = "lbServerStatus";
+            this.lbServerStatus.Size = new System.Drawing.Size(67, 13);
+            this.lbServerStatus.TabIndex = 0;
+            this.lbServerStatus.Text = "Not Running";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(493, 348);
+            this.Controls.Add(this.tbProcessLog);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.btnStartServer);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbServerStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "MainWindow";
+            this.Text = "Chat Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button btnStopServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbProcessLog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbServerStatus;
     }
 }
 
